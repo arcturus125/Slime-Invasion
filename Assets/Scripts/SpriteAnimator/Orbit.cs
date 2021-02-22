1d0bdee307a5d7268e9167b0f2b6288052d27153
jamiefp17 <57445543+jamiefp17@users.noreply.github.com>
2021-02-22 07:12:17 +0000

    Did more work on the menu. Planet has more frames in animation, and now has a moon and sun in scene as well

---
+++ Assets/Scripts/SpriteAnimator/Orbit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float orbitSpeed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up*orbitSpeed); //Every frame, rotates moon around local Y axis of the planet.
+    }
+}
---
+++ Assets/Scripts/SpriteAnimator/SpriteAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpriteAnimator
+{
+    public class SpriteAnimation : MonoBehaviour
+    {
+        public Sprite[] frames; //Array of all the frames in the animation for the planet.
+        public float timeBetweenFrames = 0.1f; //The amount of time between switching frames.
+        private float animatorDeltaTime = 0.0f; //Time since the last frame in the animator.
+        private int index = 0; //The current frame in the animation.
+        private SpriteRenderer renderer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            renderer = GetComponent<SpriteRenderer>(); //Get renderer
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (animatorDeltaTime >= timeBetweenFrames) //Iterates through frames when the waiting time between frames has been met.
+            {
+                index++;
+
+                renderer.sprite = frames[index]; //Sets sprite to current frame.
+
+                if(index >= frames.Length-1) //Loops animation if the last frame has been displayed.
+                {
+                    index = 0;
+                }
+                    
+                animatorDeltaTime -= timeBetweenFrames; //Resets animationDeltaTime ready for the next frame.
+            }
+            animatorDeltaTime += Time.deltaTime; //Tracks time.
+        }
+    }
+}
