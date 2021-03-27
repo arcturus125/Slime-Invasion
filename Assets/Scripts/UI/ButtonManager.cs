704ca06dbb9c223be76cad48352d1fb0c89ded6a
jamiefp17 <57445543+jamiefp17@users.noreply.github.com>
2021-03-27 22:17:15 +0000

    Added buttons to main menu

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void ChangeSceneButtonPress(string scene)
+    {
+        try
+        {
+            SceneManager.LoadScene(scene);
+        }
+        catch
+        {
+            Debug.Log("Couldn't switch to scene " + scene);
+        }
+    }
+}
