c2deaf2d3a23abd4e34f333ae2e172fe8ac0291b
jamiefp17 <57445543+jamiefp17@users.noreply.github.com>
2021-04-06 11:14:09 +0100

    Menu functionality increased, and script tidied and commented

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -5,15 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public void ChangeSceneButtonPress(string scene)
+    public void ChangeSceneButtonPress(string scene) //Currently used for the start button, but options could use this as well if it ends up changing to another scene.
     {
         try
         {
-            SceneManager.LoadScene(scene);
+            SceneManager.LoadScene(scene); //Attempts to load the scene from the string in the inspector.
         }
         catch
         {
-            Debug.Log("Couldn't switch to scene " + scene);
+            Debug.Log("Couldn't switch to scene " + scene); //Failed to switch, outputs a log for debugging.
         }
+    }
+
+    public void QuitApplicationButtonPress() //Quits the game.
+    {
+        Application.Quit();
     }
 }
