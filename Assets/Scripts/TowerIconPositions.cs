43c2047f367e9883c70934d62533157fa8947820
jamiefp17 <57445543+jamiefp17@users.noreply.github.com>
2021-04-15 23:34:24 +0100

    Music manager updated. It is now possible to mute music, as well as change volume and skip songs

--- Assets/Scripts/TowerIconPositions.cs
+++ Assets/Scripts/TowerIconPositions.cs
@@ -9,19 +9,17 @@ public class TowerIconPositions : MonoBehaviour
 
     void Awake()
     {
+        const float PERCENT = 10.0f; //The percent gap of the screen the icons will be placed apart.
 
         float screenWidth = Screen.width;
-        float leftSide = 0.0f - (screenWidth / 2);
-
-        float percentGap = screenWidth / 10.0f;
-
-        float currentPercentGap = percentGap;
+        float leftSide = 0.0f - (screenWidth / 2); //The left side of the screen.
+        float percentGap = screenWidth / PERCENT; //X% of the screens width.
+        float currentPercentGap = percentGap; //Counts how much to add to the left side of the screen to place the next icon.
 
         for (int i = 0; i < towerIcons.Length; i++) //Loops through icons.
         {
             float percentFromLeft = leftSide + currentPercentGap; //Works out the pos X position.
-            //towerIcons[i].transform.GetComponent<RectTransform>().rect.Set(percentFromLeft, 0.0f, 100.0f, 100.0f); //Sets the position.
-            towerIcons[i].transform.GetComponent<RectTransform>().transform.localPosition = new Vector3(percentFromLeft, 0.0f, 0.0f);
+            towerIcons[i].transform.GetComponent<RectTransform>().transform.localPosition = new Vector3(percentFromLeft, 0.0f, 0.0f); //Sets position.
             currentPercentGap += percentGap; //Changes position for next icon.
         }
         
