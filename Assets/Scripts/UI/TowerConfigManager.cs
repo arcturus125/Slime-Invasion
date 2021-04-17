e8e1738b546b01662486c3f17f969eba24ec33e6
arcturus125 <arcturus1259@gmail.com>
2021-04-17 15:50:10 +0100

    beginning to implement tower config UI
    bug fixed with clicking conveyors not bringing up their UI

---
+++ Assets/Scripts/UI/TowerConfigManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Towers;
+using UnityEngine;
+
+public class TowerConfigManager : MonoBehaviour
+{
+    public static Tower selectedTower;
+    [SerializeField]
+    private GameObject towerConfigPanel;
+    [SerializeField]
+    private TowerInventoryItem prefab;
+    [SerializeField]
+    private RectTransform contentWindow;
+    [SerializeField]
+    private GameObject scrollView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectedTower == null)
+            towerConfigPanel.SetActive(false);
+        else
+            towerConfigPanel.SetActive(true);
+    }
+}
